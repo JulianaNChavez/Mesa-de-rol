184b0aec2cd5f0e7b9917d7f936b59f271b46fb0
Juliana Chavez <Diegochavez.ayrton@gmail.com>
2023-09-28 20:42:24 -0300

    public environment

---
+++ Login/.envPUBLIC.php
@@ -0,0 +1,10 @@
+<?php
+
+function credenciales() {
+    return [
+        'usuario' => 'Quintel',
+        'clave' => 'secreto',
+        'servidor' => 'localhost',
+        'base_de_datos' => 'conexion_php',
+    ];
+}
