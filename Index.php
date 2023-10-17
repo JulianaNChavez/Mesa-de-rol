e5e2dcbe4cd1851e61dcca2f2af612a56615594e
Juliana Chavez <Diegochavez.ayrton@gmail.com>
2023-10-17 02:49:20 -0300

    Cambio de carpeta

--- Index.php
+++ Index.php
@@ -1,4 +1,30 @@
-<?php
-//Esto es una prueba en php del repositorio
-/*El trabajo consiste en crear un sistema en donde encontrar distintos sistemas de rol de mesa, pudiendo
-filtrarse por ambientacion, genero, etc. */
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Bienvenido al sistema</title>
+        <link rel="stylesheet" href="bootstrap.min.css">
+    </head>
+    <body class="container">
+      <div class="jumbotron text-center">
+      <h1>Sistema bancario</h1>
+      </div>    
+      <div class="text-center">
+        <h3>Login de usuario</h3>
+        <?php
+            if (isset($_GET['mensaje'])) {
+                echo '<div id="mensaje" class="alert alert-primary text-center">
+                    <p>'.$_GET['mensaje'].'</p></div>';
+            }
+        ?>
+
+        <form action="login.php" method="post">
+            <input name="usuario" class="form-control form-control-lg" placeholder="Usuario"><br>
+            <input name="clave" type="text" class="form-control form-control-lg" placeholder="Contraseña"><br>
+            <input type="submit" value="Ingresar" class="btn btn-primary">
+        </form><br>
+        <p><a href="create.php">Crear nuevo usuario</a></p>
+      </div> 
+    </body>
+</html>
